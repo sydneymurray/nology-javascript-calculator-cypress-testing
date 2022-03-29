49d0538d64a9cd816cb448aaafe44456d5407817
sydney.murray <sydney_i_murray@yahoo.co.uk>
2022-03-29 17:04:10 +0100

    Numbers are now displayed when key is pressed

---
+++ README.md
@@ -0,0 +1,25 @@
+ Introduction
+ This project is a nology challenge to build on online calculator
+ Live Site: 
+ GitHub: https://github.com/sydneymurray/nology-javascript-calculator
+
+ Requirements
+ HTML & CSS compatible Web Server
+
+ Recommended modules
+ Sassy CSS is required
+
+ Installation
+ Install any of the SCSS extensions in VS-Code
+
+ Configuration
+ None. 
+ This project is HTML, CSS & JavaScript only
+ 
+ Troubleshooting
+ 
+ FAQ
+ 
+ Maintainers
+ sydney Murray 
+
--- index.js
+++ index.js
@@ -1,8 +1,27 @@
+let operand = null
+let operation = null
 let display = document.querySelector("h1")
 
+function computeOperation(operation){
+    switch (expr) {
+        case "x":
+            break;
+        case "":
+            break;    
+        default:
+          console.log(operation);
+    }
+
+    console.log(operation)
+}
+
 function handleClick(event){
-    if (event.target.innerText >= "0" && event.target.innerText <= "9" )
-        console.log("Number");
+    if (event.target.innerText >= "0" && event.target.innerText <= "9" ){
+        if (display.innerText === "0")
+            display.innerText = event.target.innerText
+        else
+            display.innerText += event.target.innerText 
+    }
     else
-        console.log("Letter")
+        computeOperation(event.target.innerText)
 }
