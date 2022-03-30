71e1cee7d4814fb59b0e668b9e4c06cc092d34be
sydney.murray <sydney_i_murray@yahoo.co.uk>
2022-03-30 19:36:10 +0100

    Broke up the onClick function into 3 separate functions. numberClicked, operatorClicked & displayOperationClicked.

--- index.js
+++ index.js
@@ -1,27 +1,61 @@
 let operand = null
-let operation = null
+let previousOperation = null
 let display = document.querySelector("h1")
 
-function computeOperation(operation){
-    switch (expr) {
-        case "x":
-            break;
-        case "":
-            break;    
-        default:
-          console.log(operation);
-    }
+function operatorClicked(event){
+    if (!operand){
+        operand = display.innerText
+        previousOperation = event.target.innerText
+        display.innerText = "0"
+        return
+    }   
 
-    console.log(operation)
+    switch (operand){
+        case "x":
+            display.innerText = Number(operand) * Number(display.innerText);
+            break;
+        case "/":
+            display.innerText = Number(operand) / Number(display.innerText);
+            break;
+        case "+":
+            display.innerText = Number(operand) + Number(display.innerText);
+            break;
+        case "-":
+            display.innerText = Number(operand) - Number(display.innerText);
+            break;
+        default:
+    }
+    
+    if (event.target.innerText === "="){
+        operand = null;
+        previousOperation = null;
+    } else{
+        previousOperation = event.target.innerText
+
+    }
+    console.log("Operand: "+operand, "Operation: "+previousOperation)
+
+
+
 }
 
-function handleClick(event){
-    if (event.target.innerText >= "0" && event.target.innerText <= "9" ){
-        if (display.innerText === "0")
-            display.innerText = event.target.innerText
-        else
-            display.innerText += event.target.innerText 
-    }
+function numberClicked(event){
+    if (display.innerText === "0" || previousOperation)
+        display.innerText = event.target.innerText
     else
-        computeOperation(event.target.innerText)
+        display.innerText += event.target.innerText 
+}
+function displayOperationClicked(event){
+    switch (event.target.innerText){
+        case "C":
+            display.innerText = "0";
+            operand = null;
+            previousOperation = null;
+            return;
+        case "←":
+            display.innerText = display.innerText.slice(0, length-1);
+            return;
+        default:
+    }
+
 }
