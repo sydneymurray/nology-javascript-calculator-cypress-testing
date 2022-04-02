6d1895dbf8ea3d30d23d6608b95ee8c79b2c8826
sydney.murray <sydney_i_murray@yahoo.co.uk>
2022-04-02 03:20:17 +0100

    App now detects kepresses and computes the result

--- dist/index.dev.js
+++ dist/index.dev.js
@@ -5,7 +5,7 @@ var previousOperator = null;
 var displayRunningTotal = false;
 var display = document.querySelector("h1");
 var body = document.getElementById('body');
-document.addEventListener('keypress', keyPressed);
+document.addEventListener('keydown', keyPressed);
 
 function displayOperationClicked(event) {
   switch (event.target.innerText) {
@@ -30,6 +30,7 @@ function displayOperationClicked(event) {
       break;
 
     default:
+      return;
   }
 }
 
@@ -39,6 +40,7 @@ function numberClicked(event) {
     return;
   }
 
+  if (display.innerText.includes(".") && event.target.innerText === ".") return;
   if (display.innerText === "0" || displayRunningTotal) display.innerText = event.target.innerText;else display.innerText += event.target.innerText;
   displayRunningTotal = false;
   console.log("Operand: " + operand);
@@ -89,5 +91,63 @@ function operatorClicked(event) {
 }
 
 function keyPressed(event) {
+  var myEvent = {
+    target: {
+      innerText: event.code
+    }
+  };
   console.log("Key Pressed: " + event.code);
+  if (myEvent.target.innerText.includes("Numpad")) myEvent.target.innerText = myEvent.target.innerText.slice(6, myEvent.target.innerText.length);
+  if (myEvent.target.innerText.includes("Digit")) myEvent.target.innerText = myEvent.target.innerText.slice(5, myEvent.target.innerText.length);
+
+  switch (myEvent.target.innerText) {
+    case "Divide":
+    case "Slash":
+      myEvent.target.innerText = "/";
+      operatorClicked(myEvent);
+      return;
+
+    case "Multiply":
+      myEvent.target.innerText = "x";
+      operatorClicked(myEvent);
+      return;
+
+    case "Subtract":
+      myEvent.target.innerText = "-";
+      operatorClicked(myEvent);
+      return;
+
+    case "Add":
+      myEvent.target.innerText = "+";
+      operatorClicked(myEvent);
+      return;
+
+    case "Equal":
+    case "Enter":
+      myEvent.target.innerText = "=";
+      operatorClicked(myEvent);
+      return;
+
+    case "Period":
+    case ".":
+    case "Decimal":
+      myEvent.target.innerText = ".";
+      numberClicked(myEvent);
+      return;
+
+    case "Minus":
+      myEvent.target.innerText = "±";
+      displayOperationClicked(myEvent);
+      return;
+
+    case "Backspace":
+      myEvent.target.innerText = "←";
+      displayOperationClicked(myEvent);
+      return;
+
+    default:
+      if (myEvent.target.innerText >= "0" && myEvent.target.innerText <= "9") numberClicked(myEvent);
+  }
+
+  console.log("innerText: " + myEvent.target.innerText);
 }
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let previousOperator = null
 let displayRunningTotal = false
 const display = document.querySelector("h1")
 const body = document.getElementById('body')
-document.addEventListener('keypress', keyPressed)
+document.addEventListener('keydown', keyPressed)
 
 function displayOperationClicked(event){
     switch (event.target.innerText){
@@ -24,6 +24,7 @@ function displayOperationClicked(event){
             display.innerText = Number(display.innerText) / 100;
             break;       
         default:
+            return;
     }
 }
 
@@ -32,6 +33,9 @@ function numberClicked(event){
         display.innerText = "0."
         return
     }
+    if (display.innerText.includes(".") && event.target.innerText === ".") 
+        return
+
     if (display.innerText === "0" || displayRunningTotal )
         display.innerText = event.target.innerText
     else
@@ -82,7 +86,60 @@ function operatorClicked(event){
 }
 
 function keyPressed(event){
+    let myEvent = {target: {innerText : event.code}}
     console.log("Key Pressed: " + event.code)
+    
+    if (myEvent.target.innerText.includes("Numpad"))
+        myEvent.target.innerText = myEvent.target.innerText.slice(6, myEvent.target.innerText.length)
+    
+    if (myEvent.target.innerText.includes("Digit"))
+        myEvent.target.innerText = myEvent.target.innerText.slice(5, myEvent.target.innerText.length)
+    
+    switch(myEvent.target.innerText){
+        case "Divide":
+        case "Slash":
+            myEvent.target.innerText = "/";
+            operatorClicked(myEvent);
+            return;
+        case "Multiply":
+            myEvent.target.innerText = "x";
+            operatorClicked(myEvent);
+            return;
+        case "Subtract":
+            myEvent.target.innerText = "-";
+            operatorClicked(myEvent);
+            return;
+        case "Add":
+            myEvent.target.innerText = "+";
+            operatorClicked(myEvent);
+            return;
+        case"Equal":
+        case "Enter":
+            myEvent.target.innerText = "=";
+            operatorClicked(myEvent);
+            return;
+        case "Period":
+        case ".":
+        case "Decimal":
+            myEvent.target.innerText = ".";
+            numberClicked(myEvent);
+            return;
+        case "Minus":
+            myEvent.target.innerText = "±";
+            displayOperationClicked(myEvent);
+            return;
+        case "Backspace":
+            myEvent.target.innerText = "←";
+            displayOperationClicked(myEvent);
+            return;
+        default:
+            if(myEvent.target.innerText >= "0" && myEvent.target.innerText <="9") 
+                numberClicked(myEvent);
+    }
+    
+
+    console.log("innerText: " + myEvent.target.innerText)
+       
 }
 
 
