bd4412c2cee569a90797e1ed79e3c5a6b9b7e824
sydney.murray <sydney_i_murray@yahoo.co.uk>
2022-04-07 11:13:13 +0100

    Added some keypad tests

--- cypress/integration/dist/test.spec.dev.js
+++ cypress/integration/dist/test.spec.dev.js
@@ -5,7 +5,7 @@ describe("Does the page load", function () {
     cy.visit("http://127.0.0.1:5501/index.html");
   });
 });
-describe("Does the display show numbers", function () {
+describe("Does the display numbers when the buttons are clicked", function () {
   it("Does it display ZERO upon loading", function () {
     // Arrange
     cy.visit("http://127.0.0.1:5501/index.html"); // Assert
@@ -22,7 +22,7 @@ describe("Does the display show numbers", function () {
     cy.get("[data-cy=display]").should("have.text", "9");
   });
 });
-describe("Can it perform simple calculations", function () {
+describe("Can it perform calculations", function () {
   it("Does it display the 81 when buttons '9' 'x' '9' '=' are clicked", function () {
     // Arrange
     cy.visit("http://127.0.0.1:5501/index.html"); // Act
@@ -38,5 +38,62 @@ describe("Can it perform simple calculations", function () {
 
     cy.get("[data-cy=display]").should("have.text", "81");
   });
-}); //     cy.get(".calculator__screen").should("have.value", "11")
-// })
+  it("Does it display a running total of 49 when buttons '7' 'x' '7' '+' are clicked", function () {
+    // Arrange
+    cy.visit("http://127.0.0.1:5501/index.html"); // Act
+    // click 9
+
+    cy.get("[data-cy=7]").click(); // click x
+
+    cy.get("[data-cy=x]").click(); // click 9
+
+    cy.get("[data-cy=7]").click(); // click =
+
+    cy.get("[data-cy='+']").click(); // Assert
+
+    cy.get("[data-cy=display]").should("have.text", "49");
+  });
+  it("Does it perform a complex calculation when buttons '7' 'x' '7' '+' '100'are clicked", function () {
+    // Arrange
+    cy.visit("http://127.0.0.1:5501/index.html"); // Act
+    // click 9
+
+    cy.get("[data-cy=7]").click(); // click x
+
+    cy.get("[data-cy=x]").click(); // click 9
+
+    cy.get("[data-cy=7]").click(); // click +
+
+    cy.get("[data-cy='+']").click(); // click +
+
+    cy.get("[data-cy='1']").click(); // click +
+
+    cy.get("[data-cy='0']").click(); // click +
+
+    cy.get("[data-cy='0']").click(); // click =
+
+    cy.get("[data-cy='=']").click(); // Assert
+
+    cy.get("[data-cy=display]").should("have.text", "149");
+  });
+});
+describe("Does it detect nummeric keypad presses", function () {
+  it("Does it display 789 when the numeric keypad keys 789 are pressed", function () {
+    // Arrange
+    cy.visit("http://127.0.0.1:5501/index.html"); // Act
+    // Type 789 on the numeric keypad
+
+    cy.get("body").type('789'); // Assert
+
+    cy.get("[data-cy=display]").should("have.text", "789");
+  });
+  it("Does it display 78 when the numeric keypad keys 7488/96= are pressed", function () {
+    // Arrange
+    cy.visit("http://127.0.0.1:5501/index.html"); // Act
+    // Type 789/96= on the numeric keypad
+
+    cy.get("body").type('7488/96='); // Assert
+
+    cy.get("[data-cy=display]").should("have.text", "78");
+  });
+});
--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -4,7 +4,7 @@ describe("Does the page load",() => {
     })
 })
 
-describe("Does the display show numbers",() => {
+describe("Does the display numbers when the buttons are clicked",() => {
     it("Does it display ZERO upon loading",() => {
         // Arrange
         cy.visit("http://127.0.0.1:5501/index.html")
@@ -25,7 +25,7 @@ describe("Does the display show numbers",() => {
     })
 })
 
-describe("Can it perform simple calculations",() => {
+describe("Can it perform calculations",() => {
     it("Does it display the 81 when buttons '9' 'x' '9' '=' are clicked",() => {
         // Arrange
         cy.visit("http://127.0.0.1:5501/index.html")
@@ -46,7 +46,81 @@ describe("Can it perform simple calculations",() => {
         // Assert
         cy.get("[data-cy=display]").should("have.text", "81")
     })
+    it("Does it display a running total of 49 when buttons '7' 'x' '7' '+' are clicked",() => {
+        // Arrange
+        cy.visit("http://127.0.0.1:5501/index.html")
+        
+        // Act
+        // click 9
+        cy.get("[data-cy=7]").click()
+
+        // click x
+        cy.get("[data-cy=x]").click()
+
+        // click 9
+        cy.get("[data-cy=7]").click()
+
+        // click =
+        cy.get("[data-cy='+']").click()
+
+        // Assert
+        cy.get("[data-cy=display]").should("have.text", "49")
+    })
+    it("Does it perform a complex calculation when buttons '7' 'x' '7' '+' '100'are clicked",() => {
+        // Arrange
+        cy.visit("http://127.0.0.1:5501/index.html")
+        
+        // Act
+        // click 9
+        cy.get("[data-cy=7]").click()
+
+        // click x
+        cy.get("[data-cy=x]").click()
+
+        // click 9
+        cy.get("[data-cy=7]").click()
+
+        // click +
+        cy.get("[data-cy='+']").click()
+
+        // click +
+        cy.get("[data-cy='1']").click()
+
+        // click +
+        cy.get("[data-cy='0']").click()
+
+        // click +
+        cy.get("[data-cy='0']").click()
+
+        // click =
+        cy.get("[data-cy='=']").click()
+
+        // Assert
+        cy.get("[data-cy=display]").should("have.text", "149")
+    })
 })   
 
-    //     cy.get(".calculator__screen").should("have.value", "11")
-    // })
+describe("Does it detect nummeric keypad presses",() => {
+    it("Does it display 789 when the numeric keypad keys 789 are pressed",() => {
+        // Arrange
+        cy.visit("http://127.0.0.1:5501/index.html")
+        
+        // Act
+        // Type 789 on the numeric keypad
+        cy.get("body").type('789')
+
+        // Assert
+        cy.get("[data-cy=display]").should("have.text", "789")
+    })
+    it("Does it display 78 when the numeric keypad keys 7488/96= are pressed",() => {
+        // Arrange
+        cy.visit("http://127.0.0.1:5501/index.html")
+        
+        // Act
+        // Type 789/96= on the numeric keypad
+        cy.get("body").type('7488/96=')
+
+        // Assert
+        cy.get("[data-cy=display]").should("have.text", "78")
+    })
+})
